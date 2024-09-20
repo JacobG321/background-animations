cc7bd6e36e8e377fb26f94ca298f2d25797ca31d
JacobG321 <jacobgruber1998@gmail.com>
2024-09-20 16:16:01 -0400

    initial commit

---
+++ add-background-animations.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Add Background Animations
+ * Description: Add Background Animations to group blocks
+ * Version: 0.0.1
+ * Author Jacob Gruber
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
